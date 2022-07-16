c190468b277abfbb12d3d5c5f25d6c08d457390b
Edsey omenda <omendajames0317@gmail.com>
2022-07-17 00:07:21 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,62 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form(props) {
+  // const [firstName, setFirstName] = useState("John");
+  // const [lastName, setLastName] = useState("Henry");
+
+  // console.log(firstName)
+  // function handleFirstNameChange(event){
+  //   setFirstName(event.target.value)
+  // }
+
+  // function handleLastNameChange(event){
+  //   setLastName(event.target.value)
+  // }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={props.handleFirstNameChange} value={props.firstName} />
+      <input type="text" onChange={props.handleLastNameChange} value={props.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
 
 export default Form;
+
+
+
+// import React, {useState} from "react";
+
+// function Form(){
+
+//   const [newsLetter, setNewsLetter] = useState(false)
+
+// function handleNewsLetterChange(event){
+//   setNewsLetter(event.target.checked)
+// }
+
+// return(
+// <form>
+//   <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+//   <input type="checkbox" 
+//     id="newsletter" 
+//     onChange={handleNewsLetterChange} 
+//     checked={newsLetter}
+//   />
+// </form>
+// )
+
+// }
+
+// export default Form;
+
+
+
+
+
+
+
+
+
+
